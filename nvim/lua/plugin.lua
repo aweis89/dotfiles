b2572f78af00ac112e27d554cb53b831744c910b
Aaron Weisberg <aaronweisberg@gmail.com>
2021-12-26 21:07:03 -0800

    adds rust plugin

--- nvim/lua/plugin.lua
+++ nvim/lua/plugin.lua
@@ -4,6 +4,10 @@ return require('packer').startup(function(use)
 	use 'wbthomason/packer.nvim'
 	use 'tpope/vim-sensible'
 	use {
+		'simrat39/rust-tools.nvim',
+		config = function() require('rust-tools').setup({}) end,
+	}
+	use {
 		'kassio/neoterm',
 		config = 'vim.g.neoterm_autoinsert = 1',
 	}
@@ -18,134 +22,133 @@ return require('packer').startup(function(use)
 		event = 'VimEnter',
 	}
 	use {
-		'airblade/vim-gitgutter',
-		config = function() vim.cmd([[
-			set updatetime=250
-			let g:gitgutter_max_signs = 500
-			let g:gitgutter_map_keys = 0
-			let g:gitgutter_override_sign_column_highlight = 0
-			highlight GitGutterAdd ctermfg=2
-			highlight GitGutterChange ctermfg=3
-			highlight GitGutterDelete ctermfg=1
-			highlight GitGutterChangeDelete ctermfg=4
-			highlight HighlightedyankRegion cterm=reverse gui=reverse
-			let g:highlightedyank_highlight_duration = 150
-			let g:gitgutter_sign_allow_clobber = 0
-			]]) end,
-			event = 'VimEnter',
-		}
+	'airblade/vim-gitgutter',
+	config = function() vim.cmd([[
+		set updatetime=250
+		let g:gitgutter_max_signs = 500
+		let g:gitgutter_map_keys = 0
+		let g:gitgutter_override_sign_column_highlight = 0
+		highlight GitGutterAdd ctermfg=2
+		highlight GitGutterChange ctermfg=3
+		highlight GitGutterDelete ctermfg=1
+		highlight GitGutterChangeDelete ctermfg=4
+		highlight HighlightedyankRegion cterm=reverse gui=reverse
+		let g:highlightedyank_highlight_duration = 150
+		let g:gitgutter_sign_allow_clobber = 0
+		]]) end,
+		event = 'VimEnter',
+	}
 
-		use 'mrk21/yaml-vim'
+	use 'mrk21/yaml-vim'
 
-		use {
-			'karb94/neoscroll.nvim',
-			config = function() require('neoscroll').setup() end,
-		}
+	use {
+		'karb94/neoscroll.nvim',
+		config = function() require('neoscroll').setup() end,
+	}
 
-		use 'arkav/lualine-lsp-progress'
-		use {
-			'nvim-lualine/lualine.nvim',
-			requires = {{'arkav/lualine-lsp-progress'}},
-			config = function()
-				require('lualine').setup({
-					sections = {
-						lualine_c = {
-							'lsp_progress'
-						}
+	use 'arkav/lualine-lsp-progress'
+	use {
+		'nvim-lualine/lualine.nvim',
+		requires = {{'arkav/lualine-lsp-progress'}},
+		config = function()
+			require('lualine').setup({
+				sections = {
+					lualine_c = {
+						'lsp_progress'
 					}
-				})
-			end,
-			event = 'VimEnter',
-		}
-		use {
-			'nvim-telescope/telescope.nvim',
-			requires = {{'nvim-lua/plenary.nvim'}},
-			config = function()
-				require('telescope').setup({
-					defaults = {
-						previewer = true,
-						layout_strategy = "flex",
-					},
-				})
-				vim.cmd([[
-				nnoremap <leader>ff <cmd>Telescope find_files<cr>
-				nnoremap <leader>fg <cmd>Telescope live_grep<cr>
-				nnoremap <leader>fb <cmd>Telescope buffers<cr>
-				nnoremap <leader>fh <cmd>Telescope help_tags<cr>
-				nnoremap <leader>fa <cmd>Telescope builtin.lsp_code_actions<cr>
-				]])
-			end
-		}
+				}
+			})
+		end,
+		event = 'VimEnter',
+	}
+	use {
+		'nvim-telescope/telescope.nvim',
+		requires = {{'nvim-lua/plenary.nvim'}},
+		config = function()
+			require('telescope').setup({
+				defaults = {
+					previewer = true,
+					layout_strategy = "flex",
+				},
+			})
+			vim.cmd([[
+			nnoremap <leader>ff <cmd>Telescope find_files<cr>
+			nnoremap <leader>fg <cmd>Telescope live_grep<cr>
+			nnoremap <leader>fb <cmd>Telescope buffers<cr>
+			nnoremap <leader>fh <cmd>Telescope help_tags<cr>
+			nnoremap <leader>fa <cmd>Telescope builtin.lsp_code_actions<cr>
+			]])
+		end
+	}
 
-		-- Golang plugins
-		use 'fatih/vim-go'
-		use {
-			'ray-x/go.nvim',
-			config = function() require('go').setup() end,
-		}
-		use 'buoto/gotests-vim'
-		use 'sebdah/vim-delve'
-		use 'junegunn/fzf'
+	-- Golang plugins
+	use 'fatih/vim-go'
+	use {
+		'ray-x/go.nvim',
+		config = function() require('go').setup() end,
+	}
+	use 'buoto/gotests-vim'
+	use 'sebdah/vim-delve'
+	use 'junegunn/fzf'
 
-		use 'preservim/vimux'
-		-- github
-		use {
-			'ruanyl/vim-gh-line',
-			config = function() vim.g.gh_line_map = "<leader>hh" end,
-		}
-		use 'pwntester/octo.nvim'
-		use 'kyazdani42/nvim-web-devicons'
+	use 'preservim/vimux'
+	-- github
+	use {
+		'ruanyl/vim-gh-line',
+		config = function() vim.g.gh_line_map = "<leader>hh" end,
+	}
+	use 'pwntester/octo.nvim'
+	use 'kyazdani42/nvim-web-devicons'
 
-		-- git
-		use 'tpope/vim-fugitive'
+	-- git
+	use 'tpope/vim-fugitive'
 
-		-- LSP
-		use 'neovim/nvim-lspconfig'
-		use 'hrsh7th/cmp-nvim-lsp'
-		use 'hrsh7th/cmp-buffer'
-		use 'hrsh7th/cmp-path'
-		use 'hrsh7th/cmp-cmdline'
-		use 'ray-x/lsp_signature.nvim'
-		use 'nvim-lua/lsp-status.nvim'
-		use {
-			'williamboman/nvim-lsp-installer',
-			config = require('lsp')
-		}
-		use {
-			'hrsh7th/nvim-cmp',
-			config = require('nvim-cmp-config'),
-		}
+	-- LSP
+	use 'neovim/nvim-lspconfig'
+	use 'hrsh7th/cmp-nvim-lsp'
+	use 'hrsh7th/cmp-buffer'
+	use 'hrsh7th/cmp-path'
+	use 'hrsh7th/cmp-cmdline'
+	use 'ray-x/lsp_signature.nvim'
+	use 'nvim-lua/lsp-status.nvim'
+	use {
+		'williamboman/nvim-lsp-installer',
+		config = function() require('lsp') end,
+	}
+	use {
+		'hrsh7th/nvim-cmp',
+		config = function() require('nvim-cmp-config') end,
+	}
 
-		-- debugger
-		use 'mfussenegger/nvim-dap'
-		use 'rcarriga/nvim-dap-ui'
+	-- debugger
+	use 'mfussenegger/nvim-dap'
+	use 'rcarriga/nvim-dap-ui'
 
-		-- For vsnip users.
-		use 'hrsh7th/cmp-vsnip'
-		use 'hrsh7th/vim-vsnip'
-		use 'hrsh7th/vim-vsnip-integ'
-		use 'rafamadriz/friendly-snippets'
-		use 'nvim-treesitter/nvim-treesitter'
-		use {
-			'romgrk/nvim-treesitter-context',
-			config = function() require'treesitter-context'.setup() end,
-			requires = {{'nvim-treesitter/nvim-treesitter'}},
-		}
-		use 'tyru/current-func-info.vim'
+	-- For vsnip users.
+	use 'hrsh7th/vim-vsnip'
+	use 'hrsh7th/vim-vsnip-integ'
+	use 'rafamadriz/friendly-snippets'
+	use 'nvim-treesitter/nvim-treesitter'
+	use {
+		'romgrk/nvim-treesitter-context',
+		config = function() require'treesitter-context'.setup() end,
+		requires = {{'nvim-treesitter/nvim-treesitter'}},
+	}
+	use 'tyru/current-func-info.vim'
 
-		-- use 'folke/tokyonight.nvim', { 'branch': 'main' }
-		use 'machakann/vim-highlightedyank'
-		-- Themes
-		use 'rktjmp/lush.nvim'
-		use 'marko-cerovac/material.nvim'
-		use 'jamespwilliams/bat.vim'
-		use {
-			'ellisonleao/gruvbox.nvim',
-			config = function()
-				vim.cmd([[
-				set background=light
-				colorscheme gruvbox
-				]])
-			end,
-		}
+	-- use 'folke/tokyonight.nvim', { 'branch': 'main' }
+	use 'machakann/vim-highlightedyank'
+	-- Themes
+	use 'rktjmp/lush.nvim'
+	use 'marko-cerovac/material.nvim'
+	use 'jamespwilliams/bat.vim'
+	use {
+		'ellisonleao/gruvbox.nvim',
+		config = function()
+			vim.cmd([[
+			set background=light
+			colorscheme gruvbox
+			]])
+		end,
+	}
 	end)
