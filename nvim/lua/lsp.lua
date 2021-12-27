9d44f9f0a32c35a80f8d9ca09bd48ee95fa23ea4
Aaron Weisberg <aaronweisberg@gmail.com>
2021-12-26 16:40:18 -0800

    adds more language servers

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,3 +1,42 @@
+local myservers = {
+	cmake = {},
+	yamlls = {},
+	jsonls = {},
+	jdtls = {},
+	vimls = {},
+	java_language_server = {},
+	bashls = {},
+	golangci_lint_ls = {},
+	gopls = {},
+	pyright = {},
+	rust_analyzer = {},
+	sumneko_lua = {
+		settings = {
+			Lua = {
+				runtime = {
+					-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+					version = 'LuaJIT',
+					-- Setup your lua path
+					-- path = runtime_path,
+				},
+				diagnostics = {
+					-- Get the language server to recognize the `vim` global
+					globals = {'vim'},
+				},
+				workspace = {
+					-- Make the server aware of Neovim runtime files
+					library = vim.api.nvim_get_runtime_file("", true),
+				},
+				-- Do not send telemetry data containing a randomized but unique identifier
+				telemetry = {
+					enable = false,
+				},
+			},
+		},
+	},
+	tsserver = {},
+}
+
 -- Setup nvim-cmp.
 local cmp = require'cmp'
 
@@ -158,42 +197,6 @@ servers.register(server.Server:new {
 		cmd = { path.concat { root_dir, "golangci-lint-langserver" }},
 	},
 })
-
-local myservers = {
-	vimls = {},
-	java_language_server = {},
-	bashls = {},
-	golangci_lint_ls = {},
-	gopls = {},
-	pyright = {},
-	rust_analyzer = {},
-	sumneko_lua = {
-		settings = {
-			Lua = {
-				runtime = {
-					-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-					version = 'LuaJIT',
-					-- Setup your lua path
-					-- path = runtime_path,
-				},
-				diagnostics = {
-					-- Get the language server to recognize the `vim` global
-					globals = {'vim'},
-				},
-				workspace = {
-					-- Make the server aware of Neovim runtime files
-					library = vim.api.nvim_get_runtime_file("", true),
-				},
-				-- Do not send telemetry data containing a randomized but unique identifier
-				telemetry = {
-					enable = false,
-				},
-			},
-		},
-	},
-	tsserver = {},
-}
-
 
 for lsp, config in pairs(myservers) do
 	local default = {
