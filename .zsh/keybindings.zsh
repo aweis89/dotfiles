7d836c64fc48f84e26406a4913c8f77c5e3bf05c
Aaron Weisberg <aaronweisberg@gmail.com>
2021-12-27 19:45:47 -0800

    keybindings update

--- .zsh/keybindings.zsh
+++ .zsh/keybindings.zsh
@@ -1,57 +1,38 @@
-# give us access to ^Q
-# stty -ixon
-
-# vi mode
-bindkey -v
+bindkey "^K" kill-line
+bindkey "^N" insert-last-word
+bindkey "^P" history-search-backward
+bindkey "^Q" push-line-or-edit
+bindkey "^R" fzf-history-widget
+bindkey "^Y" accept-and-hold
+bindkey '^D' clear-screen
 bindkey '^F' autosuggest-accept
-
-# bindkey "^F" vi-cmd-mode
-
+bindkey '^J' down-history
+bindkey '^K' up-history
 bindkey '^L' end-of-line
 bindkey '^H' beginning-of-line
-bindkey '^K' up-history
-bindkey '^J' down-history
-bindkey "^K" kill-line
-# bindkey '^F' history-beginning-search-forward
-# bindkey "^R" history-incremental-search-backward
-bindkey "^P" history-search-backward
-bindkey "^Y" accept-and-hold
-bindkey "^N" insert-last-word
-bindkey "^Q" push-line-or-edit
 bindkey -s "^T" "^[Isudo ^[A" # "t" for "toughguy"
 
+# vi mode
 bindkey -v
 bindkey -M viins 'jj' vi-cmd-mode
 bindkey '^P' up-history
 bindkey '^N' down-history
-bindkey '^?' backward-delete-char
-bindkey '^h' backward-delete-char
 bindkey '^w' backward-kill-word
-# bindkey '^r' history-incremental-search-backward
 
-# zsh-autocomplete
-# Up arrow:
-bindkey '\e[A' up-line-or-search
-bindkey '\eOA' up-line-or-search
-# up-line-or-search:  Open history menu.
-# up-line-or-history: Cycle to previous history line.
+#bindkey '^?' backward-delete-char
+#bindkey '^h' backward-delete-char
+#bindkey '^w' backward-kill-word
 
-# Down arrow:
-bindkey '\e[B' down-line-or-select
-bindkey '\eOB' down-line-or-select
-# down-line-or-select:  Open completion menu.
-# down-line-or-history: Cycle to next history line.
-
-# Control-Space:
-bindkey '\0' list-expand
-# list-expand:      Reveal hidden completions.
-# set-mark-command: Activate text selection.
-
-# Uncomment the following lines to disable live history search:
-# zle -A {.,}history-incremental-search-forward
-# zle -A {.,}history-incremental-search-backward
-
-# Return key in completion menu & history menu:
-bindkey -M menuselect '\r' .accept-line
-# .accept-line: Accept command line.
-# accept-line:  Accept selection and exit menu.
+## Up arrow:
+#bindkey '\e[A' up-line-or-search
+#bindkey '\eOA' up-line-or-search
+## up-line-or-search:  Open history menu.
+## up-line-or-history: Cycle to previous history line.
+#
+## Down arrow:
+#bindkey '\e[B' down-line-or-select
+#bindkey '\eOB' down-line-or-select
+#
+## Control-Space:
+#bindkey '\0' list-expand
+#bindkey -M menuselect '\r' .accept-line
