25b297ff264e1bc8e2e1c3e0e5a36d2c6526f599
Aaron Weisberg <aaronweisberg@gmail.com>
2021-12-27 18:26:39 -0800

    adds zsh files

---
+++ .zsh/keybindings.zsh
@@ -0,0 +1,57 @@
+# give us access to ^Q
+# stty -ixon
+
+# vi mode
+bindkey -v
+bindkey '^F' autosuggest-accept
+
+# bindkey "^F" vi-cmd-mode
+
+bindkey '^L' end-of-line
+bindkey '^H' beginning-of-line
+bindkey '^K' up-history
+bindkey '^J' down-history
+bindkey "^K" kill-line
+# bindkey '^F' history-beginning-search-forward
+# bindkey "^R" history-incremental-search-backward
+bindkey "^P" history-search-backward
+bindkey "^Y" accept-and-hold
+bindkey "^N" insert-last-word
+bindkey "^Q" push-line-or-edit
+bindkey -s "^T" "^[Isudo ^[A" # "t" for "toughguy"
+
+bindkey -v
+bindkey -M viins 'jj' vi-cmd-mode
+bindkey '^P' up-history
+bindkey '^N' down-history
+bindkey '^?' backward-delete-char
+bindkey '^h' backward-delete-char
+bindkey '^w' backward-kill-word
+# bindkey '^r' history-incremental-search-backward
+
+# zsh-autocomplete
+# Up arrow:
+bindkey '\e[A' up-line-or-search
+bindkey '\eOA' up-line-or-search
+# up-line-or-search:  Open history menu.
+# up-line-or-history: Cycle to previous history line.
+
+# Down arrow:
+bindkey '\e[B' down-line-or-select
+bindkey '\eOB' down-line-or-select
+# down-line-or-select:  Open completion menu.
+# down-line-or-history: Cycle to next history line.
+
+# Control-Space:
+bindkey '\0' list-expand
+# list-expand:      Reveal hidden completions.
+# set-mark-command: Activate text selection.
+
+# Uncomment the following lines to disable live history search:
+# zle -A {.,}history-incremental-search-forward
+# zle -A {.,}history-incremental-search-backward
+
+# Return key in completion menu & history menu:
+bindkey -M menuselect '\r' .accept-line
+# .accept-line: Accept command line.
+# accept-line:  Accept selection and exit menu.
