a22e91200afd1358f1e733a84c93fdd8a02d99c4
Aaron Weisberg <aaronweisberg@gmail.com>
2022-01-06 12:18:15 -0800

    use float windows instead of virtual text

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -93,9 +93,20 @@ for lsp, config in pairs(myservers) do
 end
 
 vim.diagnostic.config({
-	virtual_text = true,
+	float = {
+		show_header = false,
+		source = 'always',
+		border = 'rounded',
+		focusable = true,
+	},
+	virtual_text = false,
 	signs = true,
 	underline = true,
 	update_in_insert = true,
-	severity_sort = false,
+	severity_sort = true,
 })
+
+-- You will likely want to reduce updatetime which affects CursorHold
+-- note: this setting is global and should be set only once
+vim.o.updatetime = 250
+vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
