01671548241a26bbc12ffb79cf5ed454ec1df2d7
Aaron Weisberg <aaronweisberg@gmail.com>
2022-05-03 12:42:42 -0700

    adds lsp mappings to init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -37,6 +37,25 @@ map('t', 'jj', [[<C-\><C-n>]])
 map('t', 'qq', [[<C-\><C-n>:q!<CR>]])
 map('t', '<Esc>', [[<C-\><C-n>:q!<CR>]])
 
+map('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>')
+map('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
+map('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>')
+map('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>')
+map('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>')
+map('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>')
+map('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>')
+map('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>')
+map('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>')
+map('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>')
+map('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>')
+map('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>')
+map('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>')
+map('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>')
+map('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>')
+map('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>')
+map('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>')
+map('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>')
+
 vim.cmd([[
     autocmd TermOpen * startinsert
     
