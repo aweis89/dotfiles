2467cc0dd5b9c04412b6b69e4df452cd1603675d
Aaron Weisberg <aaronweisberg@gmail.com>
2022-05-25 10:52:59 -0700

    use tmux height

--- .zsh/kubectl.zsh
+++ .zsh/kubectl.zsh
@@ -3,26 +3,36 @@ alias kcu=kubectx
 
 kf() {
     resource=${1:-pods}
-    local name=$(kubectl get $resource --no-headers | fzf | awk '{print $1}')
+    local name=$(kubectl get $resource --no-headers ${@:2} | fzf | awk '{print $1}')
     yaml=$(kubectl get $resource $name -o yaml)
     echo $yaml | fzf_stdin_preview
 }
 
 kfa() {
     resource=${1:-pods}
-    local line=$(kubectl get $resource --no-headers -A | fzf -0)
+    local line=$(kubectl get $resource --no-headers -A ${@:2} | fzf)
     namespace=$(echo $line | awk '{print $1}')
     name=$(echo $line | awk '{print $2}')
     yaml=$(kubectl get $resource $name -n $namespace -o yaml)
     echo $yaml | fzf_stdin_preview
 }
 
+BAT_THEME=${BAT_THEME:-'OneHalfDark'}
+
 fzf_stdin_preview() {
-    stdin=$(cat /dev/stdin)
     lang=${1:-yaml}
+    surround=20
+    if test -n "$TMUX"
+    then
+        height=$(tmux display-message -p '#{pane_height}')
+        surround=$((height - 15))
+    fi
+    stdin=$(cat /dev/stdin)
     tmpfile=$(mktemp $HOME/tmp/fzfstin.XXXX)
+    trap "rm -f $tmpfile" EXIT
     echo "$stdin" > $tmpfile
-    preview_cmd="cat $tmpfile | head -n \$((10 + {n})) | tail -n 20 | bat --language=$lang --color=always --highlight-line=11 --theme OneHalfDark"
+    preview_cmd="cat $tmpfile | head -n \$(($surround+{n})) | \
+        tail -n \$(($surround*2)) | bat --language=$lang --color=always \
+        --highlight-line=\$(($surround+1)) --theme $BAT_THEME"
     echo $stdin | fzf --preview "$preview_cmd"
-    rm $tmpfile
 }
