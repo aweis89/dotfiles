965de50eeaac6de1a02093f7aa65e8af2417a587
Aaron Weisberg <aaronweisberg@gmail.com>
2022-06-01 18:38:28 -0700

    ignore phpdoc

--- nvim/lua/plugin.lua
+++ nvim/lua/plugin.lua
@@ -313,8 +313,8 @@ return require('packer').startup(function(use)
                 -- Install languages synchronously (only applied to `ensure_installed`)
                 sync_install = false,
 
-                -- List of parsers to ignore installing
-                ignore_install = { "javascript" },
+                -- phpdoc doesn't work on m1 chips
+                ignore_install = { "phpdoc" },
 
                 highlight = {
                     -- `false` will disable the whole extension
