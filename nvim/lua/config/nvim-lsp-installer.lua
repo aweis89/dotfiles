a85c7cb10dbe4133671e422703429ef56459ee7d
Aaron Weisberg <aaronweisberg@gmail.com>
2022-07-01 19:41:10 -0700

    update

--- nvim/lua/config/nvim-lsp-installer.lua
+++ nvim/lua/config/nvim-lsp-installer.lua
@@ -14,98 +14,98 @@ local border = {
 -- key is server name and value is options used by lspconfig
 -- nvim-lsp-installer will also install and setup if it has installer
 local server_configs = {
-    cmake = {},
-    -- yamlls = {},
-    jsonls = {},
-    jdtls = {},
-    vimls = {},
-    java_language_server = {},
-    bashls = {},
-    golangci_lint_ls = {},
-    gopls = {},
-    -- gopls = {
-    --     -- capabilities = cap,
-    --     filetypes = { 'go', 'gomod', 'gohtmltmpl', 'gotexttmpl' },
-    --     message_level = vim.lsp.protocol.MessageType.Error,
-    --     cmd = {
-    --         'gopls', -- share the gopls instance if there is one already
-    --         '-remote=auto', --[[ debug options ]] --
-    --         -- "-logfile=auto",
-    --         -- "-debug=:0",
-    --         '-remote.debug=:0',
-    --         -- "-rpc.trace",
-    --     },
-    --     flags = { allow_incremental_sync = true, debounce_text_changes = 1000 },
-    --     settings = {
-    --         gopls = {
-    --             -- more settings: https://github.com/golang/tools/blob/master/gopls/doc/settings.md
-    --             -- not supported
-    --             analyses = { unusedparams = true, unreachable = false },
-    --             codelenses = {
-    --                 generate = true, -- show the `go generate` lens.
-    --                 gc_details = false, --  // Show a code lens toggling the display of gc's choices.
-    --                 test = true,
-    --                 tidy = true,
-    --             },
-    --             usePlaceholders = true,
-    --             completeUnimported = true,
-    --             staticcheck = true,
-    --             matcher = 'fuzzy',
-    --             diagnosticsDelay = '500ms',
-    --             experimentalWatchedFileDelay = '1000ms',
-    --             symbolMatcher = 'fuzzy',
-    --             gofumpt = true, -- true, -- turn on for new repos, gofmpt is good but also create code turmoils
-    --             buildFlags = { '-tags', 'integration' },
-    --             -- buildFlags = {"-tags", "functional"}
-    --         },
-    --     },
-    -- },
+	cmake = {},
+	-- yamlls = {},
+	jsonls = {},
+	jdtls = {},
+	vimls = {},
+	java_language_server = {},
+	bashls = {},
+	golangci_lint_ls = {},
+	gopls = {},
+	-- gopls = {
+	--     -- capabilities = cap,
+	--     filetypes = { 'go', 'gomod', 'gohtmltmpl', 'gotexttmpl' },
+	--     message_level = vim.lsp.protocol.MessageType.Error,
+	--     cmd = {
+	--         'gopls', -- share the gopls instance if there is one already
+	--         '-remote=auto', --[[ debug options ]] --
+	--         -- "-logfile=auto",
+	--         -- "-debug=:0",
+	--         '-remote.debug=:0',
+	--         -- "-rpc.trace",
+	--     },
+	--     flags = { allow_incremental_sync = true, debounce_text_changes = 1000 },
+	--     settings = {
+	--         gopls = {
+	--             -- more settings: https://github.com/golang/tools/blob/master/gopls/doc/settings.md
+	--             -- not supported
+	--             analyses = { unusedparams = true, unreachable = false },
+	--             codelenses = {
+	--                 generate = true, -- show the `go generate` lens.
+	--                 gc_details = false, --  // Show a code lens toggling the display of gc's choices.
+	--                 test = true,
+	--                 tidy = true,
+	--             },
+	--             usePlaceholders = true,
+	--             completeUnimported = true,
+	--             staticcheck = true,
+	--             matcher = 'fuzzy',
+	--             diagnosticsDelay = '500ms',
+	--             experimentalWatchedFileDelay = '1000ms',
+	--             symbolMatcher = 'fuzzy',
+	--             gofumpt = true, -- true, -- turn on for new repos, gofmpt is good but also create code turmoils
+	--             buildFlags = { '-tags', 'integration' },
+	--             -- buildFlags = {"-tags", "functional"}
+	--         },
+	--     },
+	-- },
 	--
 
-    solargraph = {
-        settings = {
-            solargraph = {
-                diagnostics = true,
-                completion = true,
-                autoformat = true,
-                folding = true,
-                references = true,
-                rename = true,
-                symbols = true,
-            }
-        }
-    },
+	solargraph = {
+		settings = {
+			solargraph = {
+				diagnostics = true,
+				completion = true,
+				autoformat = true,
+				folding = true,
+				references = true,
+				rename = true,
+				symbols = true,
+			}
+		}
+	},
 
-    pyright = {},
-    rust_analyzer = {},
-    sumneko_lua = {
-        settings = {
-            Lua = {
-                runtime = {
-                    -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-                    version = 'LuaJIT',
-                    -- Setup your lua path
-                    -- path = runtime_path,
-                },
-                diagnostics = {
-                    -- Get the language server to recognize the `vim` global
-                    globals = {'vim', 'hs'},
-                },
-                workspace = {
-                    -- Make the server aware of Neovim runtime files
-                    library = {
-                        vim.api.nvim_get_runtime_file("", true),
-                        string.format('%s/.hammerspoon/Spoons/EmmyLua.spoon/annotations', os.getenv 'HOME'),
-                    }
-                },
-                -- Do not send telemetry data containing a randomized but unique identifier
-                telemetry = {
-                    enable = false,
-                },
-            },
-        },
-    },
-    tsserver = {},
+	pyright = {},
+	rust_analyzer = {},
+	sumneko_lua = {
+		settings = {
+			Lua = {
+				runtime = {
+					-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+					version = 'LuaJIT',
+					-- Setup your lua path
+					-- path = runtime_path,
+				},
+				diagnostics = {
+					-- Get the language server to recognize the `vim` global
+					globals = { 'vim', 'hs' },
+				},
+				workspace = {
+					-- Make the server aware of Neovim runtime files
+					library = {
+						vim.api.nvim_get_runtime_file("", true),
+						string.format('%s/.hammerspoon/Spoons/EmmyLua.spoon/annotations', os.getenv 'HOME'),
+					}
+				},
+				-- Do not send telemetry data containing a randomized but unique identifier
+				telemetry = {
+					enable = false,
+				},
+			},
+		},
+	},
+	tsserver = {},
 }
 
 -- To instead override globally
@@ -123,7 +123,7 @@ local on_attach = function(client, bufnr)
 	local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
 
 	-- Mappings.
-	local opts = { noremap=true, silent=true }
+	local opts = { noremap = true, silent = true }
 	buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
 	buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
 	buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
@@ -167,7 +167,7 @@ capabilities.textDocument.completion.completionItem.resolveSupport = {
 local lsp_installer_servers = require('nvim-lsp-installer.servers')
 
 local M = {}
-function M.setup ()
+function M.setup()
 	for lsp, config in pairs(server_configs) do
 		local default = {
 			on_attach = on_attach,
@@ -183,7 +183,7 @@ function M.setup ()
 
 		local server_available, requested_server = lsp_installer_servers.get_server(lsp)
 		if server_available then
-			requested_server:on_ready(function ()
+			requested_server:on_ready(function()
 				requested_server:setup(config)
 			end)
 			if not requested_server:is_installed() then
