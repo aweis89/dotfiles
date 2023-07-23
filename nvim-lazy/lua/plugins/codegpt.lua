5fa0f3b1a9caef07b61541790c622c47d906f7f9
Aaron Weisberg <aaronweisberg@gmail.com>
2023-07-22 21:57:50 -0700

    feat: Add CodeGPT.nvim plugin configuration

---
+++ nvim-lazy/lua/plugins/codegpt.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "dpayne/CodeGPT.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+    },
+    config = function()
+      require("codegpt.config")
+    end,
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    opts = function(_, opts)
+      local CodeGPTModule = require("codegpt")
+      table.insert(opts.sections.lualine_x, { CodeGPTModule.get_status, "encoding", "fileformat" })
+    end,
+  },
+}
