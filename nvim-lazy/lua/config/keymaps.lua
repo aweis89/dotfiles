3b27dcac3d6dd11edff096e6eeb3dd67d233cd0d
Aaron Weisberg <aweisberg@c02g8bmqmd6r.hsd1.wa.comcast.net>
2023-07-29 20:54:25 -0700

    adds keymaps

--- nvim-lazy/lua/config/keymaps.lua
+++ nvim-lazy/lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local function map(mode, lhs, rhs, opts)
+  opts = opts or {}
+  opts.silent = opts.silent ~= false
+  vim.keymap.set(mode, lhs, rhs, opts)
+end
+
+map("n", "L", "$", { desc = "Go to end of line", remap = true })
+map("n", "H", "^", { desc = "Go to begining of line", remap = true })
