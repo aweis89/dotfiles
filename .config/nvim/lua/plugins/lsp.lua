138a34c69b3a5c565cb91ae1865e36fb098b1cb6
Aaron Weisberg <aaronweisberg@gmail.com>
2023-08-04 12:53:20 -0700

    update gopls

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -117,15 +117,8 @@ return {
           -- capabilities = cap,
           filetypes = { "go", "gomod", "gohtmltmpl", "gotexttmpl" },
           message_level = vim.lsp.protocol.MessageType.Error,
-          cmd = {
-            "gopls", -- share the gopls instance if there is one already
-            "-remote=auto", --[[ debug options ]] --
-            -- "-logfile=auto",
-            -- "-debug=:0",
-            "-remote.debug=:0",
-            -- "-rpc.trace",
-          },
           flags = { allow_incremental_sync = true, debounce_text_changes = 1000 },
+          hints = false,
           settings = {
             gopls = {
               -- more settings: https://github.com/golang/tools/blob/master/gopls/doc/settings.md
@@ -145,6 +138,15 @@ return {
               symbolMatcher = "fuzzy",
               gofumpt = false, -- true, -- turn on for new repos, gofmpt is good but also create code turmoils
               buildFlags = { "-tags", "integration" },
+              hints = {
+                assignVariableTypes = false,
+                compositeLiteralFields = false,
+                compositeLiteralTypes = false,
+                constantValues = false,
+                functionTypeParameters = false,
+                parameterNames = false,
+                rangeVariableTypes = false,
+              },
             },
           },
         },
