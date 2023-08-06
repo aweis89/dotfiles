2f2c036e17e719d61ce42a4ab5323a694c24d3c3
Aaron Weisberg <aaronweisberg@gmail.com>
2023-08-05 19:11:37 -0700

    disable bufferline

--- .config/nvim/lua/plugins/golang.lua
+++ .config/nvim/lua/plugins/golang.lua
@@ -14,27 +14,11 @@ return {
       "nvim-treesitter/nvim-treesitter",
     },
     config = function()
-      local function org_imports()
-        local clients = vim.lsp.get_active_clients()
-        for _, client in pairs(clients) do
-          local params = vim.lsp.util.make_range_params(nil, client.offset_encoding)
-          params.context = { only = { "source.organizeImports" } }
-
-          local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 5000)
-          for _, res in pairs(result or {}) do
-            for _, r in pairs(res.result or {}) do
-              if r.edit then
-                vim.lsp.util.apply_workspace_edit(r.edit, client.offset_encoding)
-              else
-                vim.lsp.buf.execute_command(r.command)
-              end
-            end
-          end
-        end
-      end
       vim.api.nvim_create_autocmd("BufWritePre", {
         pattern = { "*.go" },
-        callback = org_imports,
+        callback = function()
+          vim.cmd("GoImport")
+        end,
       })
 
       require("go").setup()
