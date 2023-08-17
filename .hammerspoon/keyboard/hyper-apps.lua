ea8554918f75c3c22f4047ebe2de15356f56fbd1
Aaron Weisberg <aaronweisberg@gmail.com>
2023-08-17 10:59:22 -0700

    change window modifiers

--- .hammerspoon/keyboard/hyper-apps.lua
+++ .hammerspoon/keyboard/hyper-apps.lua
@@ -1,8 +1,6 @@
 -- Default keybindings for launching apps in Hyper Mode
---
--- To launch _your_ most commonly-used apps via Hyper Mode, create a copy of
--- this file, save it as `hyper-apps.lua`, and edit the table below to configure
--- your preferred shortcuts.
+hs.application.enableSpotlightForNameSearches(true)
+
 return {
 	{ "d", "Deezer" }, -- "M" for "Music"
 	{ "b", "Arc" }, -- "B" for "Browser"
