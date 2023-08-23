d9fbc329858fa675ffbeaedf4287dc54950ff2ad
Aaron Weisberg <aaronweisberg@gmail.com>
2023-08-22 17:38:40 -0700

    fix GithubBrowse

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -8,28 +8,10 @@ local function map(mode, lhs, rhs, opts)
   vim.keymap.set(mode, lhs, rhs, opts)
 end
 
-local function get_relative_path_to_repo_root()
-  local handle = io.popen('git rev-parse --show-prefix')
-  local git_prefix = handle:read("*a"):gsub("%s+$", "") -- Remove trailing whitespace, including newline
-  handle:close()
-
-  if git_prefix == "" then
-    -- Either not in a git repo or in the root of the repo
-    return vim.fn.expand('%')
-  else
-    return git_prefix .. vim.fn.expand('%:t')
-  end
-end
-
-local function echo(str)
-  vim.api.nvim_echo({ { str, None } }, false, {})
-end
-
 function GithubBrowse()
-  local repo_path = get_relative_path_to_repo_root()
-  local current_line = vim.api.nvim_win_get_cursor(0)[1]
-  local handle = io.popen('gh browse ' .. repo_path .. ':' .. current_line)
-  handle:close()
+  local path = vim.fn.expand('%:t')
+  local line = vim.api.nvim_win_get_cursor(0)[1]
+  os.execute('gh browse ' .. path .. ':' .. line)
 end
 
 map("n", "L", "$", { desc = "Go to end of line", remap = true })
