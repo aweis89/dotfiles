d25084523a0f4d5df4243c8a4acd4a176d648d7c
Aaron Weisberg <aaronweisberg@gmail.com>
2023-09-11 14:22:45 -0700

    add additional slack workspace

--- .hammerspoon/keyboard/hyper-apps.lua
+++ .hammerspoon/keyboard/hyper-apps.lua
@@ -2,32 +2,39 @@
 hs.application.enableSpotlightForNameSearches(true)
 
 return {
-	{ "d", "Deezer" }, -- "M" for "Music"
-	{ "b", "Arc" }, -- "B" for "Browser"
-	{ "f", "Finder" }, -- "F" for "Finder"
-	{ "s", "Slack" }, -- "S" for "Slack"
-	{ "t", "kitty" }, -- "T" for "Terminal"
-	{ "c", "calendar" }, -- "C" for "Calendar"
-	{ "z", "zoom.us" }, -- "Z" for "Zoom"
-	{
-		"m",
-		"Slack", -- Slack workspace #2
-		function()
-			hs.eventtap.keyStroke({ "cmd" }, "2")
-		end,
-	},
-	{
-		"s",
-		"Slack", -- Slack workspace #1
-		function()
-			hs.eventtap.keyStroke({ "cmd" }, "1")
-		end,
-	},
-	{
-		"g", -- ChatGPT
-		"Arc",
-		function()
-			hs.eventtap.keyStroke({ "cmd" }, "3")
-		end,
-	},
+  { "d", "Deezer" },   -- "M" for "Music"
+  { "b", "Arc" },      -- "B" for "Browser"
+  { "f", "Finder" },   -- "F" for "Finder"
+  { "s", "Slack" },    -- "S" for "Slack"
+  { "t", "kitty" },    -- "T" for "Terminal"
+  { "c", "calendar" }, -- "C" for "Calendar"
+  { "z", "zoom.us" },  -- "Z" for "Zoom"
+  {
+    "s",
+    "Slack", -- Slack workspace #1
+    function()
+      hs.eventtap.keyStroke({ "cmd" }, "1")
+    end,
+  },
+  {
+    "m",
+    "Slack", -- Slack workspace #2
+    function()
+      hs.eventtap.keyStroke({ "cmd" }, "2")
+    end,
+  },
+  {
+    "q",
+    "Slack", -- Slack workspace #3
+    function()
+      hs.eventtap.keyStroke({ "cmd" }, "3")
+    end,
+  },
+  {
+    "g", -- ChatGPT
+    "Arc",
+    function()
+      hs.eventtap.keyStroke({ "cmd" }, "3")
+    end,
+  },
 }
