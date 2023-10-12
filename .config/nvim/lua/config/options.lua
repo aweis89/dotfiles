fa3c60ed01f846b27f612185ba0cc8b2fa834760
Aaron Weisberg <aaronweisberg@gmail.com>
2023-10-12 14:33:02 -0700

    update tmux split from within vim

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+local settings = {
+  "set norelativenumber",
+}
+
+for _, value in ipairs(settings) do
+  vim.api.nvim_exec(value, nil)
+end
