a612bac5bcd1f8f259a1182a38959c85fa95ca1d
Aaron Weisberg <aaronweisberg@gmail.com>
2024-03-16 17:42:13 -0700

    update copilot mappings

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -10,7 +10,21 @@ return {
     config = function()
       local default_opts = require("CopilotChat.config")
       local user_opts = {
-        context = "buffer", -- Context to use, 'buffers', 'buffer' or 'manual'
+        context = "buffers", -- Context to use, 'buffers', 'buffer' or 'manual'
+        mappings = {
+          close = "q",
+          reset = "<C-l>",
+          complete = "<C-g>",
+          submit_prompt = "<C-s>",
+          accept_diff = "<C-y>",
+          show_diff = "<C-d>",
+        },
+        prompts = {
+          Improve = {
+            prompt = "/COPILOT_REFACTOR can this be improved?",
+            selection = require("CopilotChat.select").buffer,
+          },
+        },
       }
       local final_opts = vim.tbl_deep_extend("force", default_opts, user_opts)
       require("CopilotChat").setup(final_opts)
@@ -19,24 +33,26 @@ return {
     keys = {
       -- Telescope shortcuts
       {
-        "<leader>cch",
+        "<leader>ch",
         function()
           local actions = require("CopilotChat.actions")
           require("CopilotChat.integrations.telescope").pick(actions.help_actions())
         end,
         desc = "CopilotChat - Help actions",
+        remap = true,
       },
       -- Show prompts actions with telescope
       {
-        "<leader>ccp",
+        "<leader>cp",
         function()
           local actions = require("CopilotChat.actions")
           require("CopilotChat.integrations.telescope").pick(actions.prompt_actions())
         end,
         desc = "CopilotChat - Prompt actions",
+        remap = true,
       },
       {
-        "<leader>ccb",
+        "<leader>cb",
         function()
           local input = vim.fn.input("Quick Chat: ")
           if input ~= "" then
@@ -44,62 +60,21 @@ return {
           end
         end,
         desc = "CopilotChat - Quick chat",
+        remap = true,
       },
       {
-        "<leader>ccq",
+        "<leader>cq",
         "<cmd>CopilotChat<cr>",
         desc = "CopilotChat - Quick chat",
         mode = { "n", "v" },
+        remap = true,
+      },
+      {
+        "<leader>cr",
+        "<cmd>CopilotChatImprove<cr>",
+        desc = "CopilotChat - Refactor",
+        remap = true,
       },
     },
-  },
-  {
-    "robitx/gp.nvim",
-    config = function()
-      require("gp").setup({
-        agents = {
-          -- Disable ChatGPT 3.5
-          {
-            name = "ChatGPT3-5",
-            chat = false, -- just name would suffice
-            command = false, -- just name would suffice
-          },
-          {
-            name = "ChatGPT4",
-            chat = true,
-            command = true,
-            -- string with model name or table with model name and parameters
-            model = { model = "gpt-4-1106-preview", temperature = 1.1, top_p = 1 },
-            -- system prompt (use this to specify the persona/role of the AI)
-            system_prompt = "You are a general AI assistant.\n\n"
-              .. "The user provided the additional info about how they would like you to respond:\n\n"
-              .. "- If you're unsure don't guess and say you don't know instead.\n"
-              .. "- Ask question if you need clarification to provide better answer.\n"
-              .. "- Think deeply and carefully from first principles step by step.\n"
-              .. "- Zoom out first to see the big picture and then zoom in to details.\n"
-              .. "- Use Socratic method to improve your thinking and coding skills.\n"
-              .. "- Don't elide any code from your output if the answer requires coding.\n"
-              .. "- Take a deep breath; You've got this!\n",
-          },
-        },
-      })
-      -- shortcuts might be setup here (see Usage > Shortcuts in Readme)
-    end,
-  },
-  {
-    "james1236/backseat.nvim",
-    config = function()
-      require("backseat").setup({
-        -- Alternatively, set the env var $OPENAI_API_KEY by putting "export OPENAI_API_KEY=sk-xxxxx" in your ~/.bashrc
-        openai_model_id = "gpt-4", --gpt-4 (If you do not have access to a model, it says "The model does not exist")
-        -- language = 'english', -- Such as 'japanese', 'french', 'pirate', 'LOLCAT'
-        -- split_threshold = 100,
-        -- additional_instruction = "Respond snarkily", -- (GPT-3 will probably deny this request, but GPT-4 complies)
-        highlight = {
-          icon = " ", -- ''
-          -- group = 'Comment',
-        },
-      })
-    end,
   },
 }
