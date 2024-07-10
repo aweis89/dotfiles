5804987e9551095b7376d589204890ad87148c7f
Aaron Weisberg <aaronweisberg@gmail.com>
2024-07-10 14:39:27 -0700

    adds ConformEnable/ConformDisable

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,13 +1,33 @@
 return {
   {
     "stevearc/conform.nvim",
-    opts = {
-      formatters_by_ft = {
-        go = { "gofumpt", "goimports", "golines" },
-        tf = { "terraform_fmt" },
-        yaml = { "yamlfmt" },
-        lua = { "stylua" },
-      },
-    },
+    init = function()
+      vim.api.nvim_create_user_command("ConformDisable", function(args)
+        if args.bang then
+          -- FormatDisable! will disable formatting just for this buffer
+          vim.b.disable_autoformat = true
+        else
+          vim.g.disable_autoformat = true
+        end
+      end, {
+        desc = "Disable autoformat-on-save",
+        bang = true,
+      })
+
+      vim.api.nvim_create_user_command("ConformEnable", function()
+        vim.b.disable_autoformat = false
+        vim.g.disable_autoformat = false
+      end, {
+        desc = "Re-enable autoformat-on-save",
+      })
+    end,
+    opts = function(_, opts)
+      local ft = opts.formatters_by_ft
+      ft.go = { "gofumpt", "goimports", "golines" }
+      ft.tf = { "terraform_fmt" }
+      ft.yaml = { "yamlfmt" }
+      ft.lua = { "stylua" }
+      return opts
+    end,
   },
 }
