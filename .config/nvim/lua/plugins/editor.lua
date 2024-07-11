d47ace7b6a719d215583da4536473441a4d4440f
Aaron Weisberg <aaronweisberg@gmail.com>
2024-07-11 13:34:00 -0700

    update

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -130,6 +130,21 @@ return {
           },
         },
       })
+
+      opts.default_component_configs = {
+        indent = {
+          with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+          expander_collapsed = "",
+          expander_expanded = "",
+          expander_highlight = "NeoTreeExpander",
+        },
+        git_status = {
+          symbols = {
+            unstaged = "",
+            staged = "",
+          },
+        },
+      }
     end,
     keys = {
       {
