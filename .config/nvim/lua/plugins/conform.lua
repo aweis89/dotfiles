ebf8040f1f2565a8e4f5031319087bd5227afced
Aaron Weisberg <aaronweisberg@gmail.com>
2024-09-03 14:18:33 -0700

    remove conform init

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,26 +1,26 @@
 return {
   {
     "stevearc/conform.nvim",
-    init = function()
-      vim.api.nvim_create_user_command("ConformDisable", function(args)
-        if args.bang then
-          -- FormatDisable! will disable formatting just for this buffer
-          vim.b.disable_autoformat = true
-        else
-          vim.g.disable_autoformat = true
-        end
-      end, {
-        desc = "Disable autoformat-on-save",
-        bang = true,
-      })
+    -- init = function()
+    --   vim.api.nvim_create_user_command("ConformDisable", function(args)
+    --     if args.bang then
+    --       -- FormatDisable! will disable formatting just for this buffer
+    --       vim.b.disable_autoformat = true
+    --     else
+    --       vim.g.disable_autoformat = true
+    --     end
+    --   end, {
+    --     desc = "Disable autoformat-on-save",
+    --     bang = true,
+    --   })
 
-      vim.api.nvim_create_user_command("ConformEnable", function()
-        vim.b.disable_autoformat = false
-        vim.g.disable_autoformat = false
-      end, {
-        desc = "Re-enable autoformat-on-save",
-      })
-    end,
+    --   vim.api.nvim_create_user_command("ConformEnable", function()
+    --     vim.b.disable_autoformat = false
+    --     vim.g.disable_autoformat = false
+    --   end, {
+    --     desc = "Re-enable autoformat-on-save",
+    --   })
+    -- end,
     opts = function(_, opts)
       local ft = opts.formatters_by_ft
       ft.go = { "gofumpt", "goimports", "golines" }
