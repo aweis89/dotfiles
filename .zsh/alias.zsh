665b82a03f494881674c3408895d4d45b49284d8
Aaron Weisberg <aaronweisberg@gmail.com>
2024-10-11 10:26:50 -0700

    update .zsh and hammerspoon iterm

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -3,7 +3,7 @@ alias gos=go-search
 alias hf=helmfile
 alias int='curl -Ss https://google.com'
 alias k=kubectl
-alias kb=kubebuilder 
+alias kb=kubebuilder
 alias kcg='kubectl config get-contexts'
 alias kp=kube-prompt
 alias kw='watch kubectl'
@@ -14,20 +14,19 @@ alias tf=terraform
 alias tmux="TERM=screen-256color tmux"
 alias tmuxs='vim ~/.config/tmux/tmux.conf'
 alias tt=gotestsum
-alias vim='nvim'
 alias vims='cd ~/.config/nvim/lua && vim'
 alias zshs='vim ~/.zshrc'
 alias ff='find . -type f -name'
 alias fd='find . -type d -name'
 
 ggmain() {
-  git checkout main 2> /dev/null || git checkout master
+  git checkout main 2>/dev/null || git checkout master
   ggpull
 }
 
 goinit() {
-    local name=$1
-    [[ -d $name ]] || mkdir $name
-    cd $name
-    go mod init github.com/aweis89/$name
+  local name=$1
+  [[ -d $name ]] || mkdir $name
+  cd $name
+  go mod init github.com/aweis89/$name
 }
