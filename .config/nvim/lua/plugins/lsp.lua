6e7c37808b956f7963297ef5dd393d63ef626fb7
Aaron Weisberg <aaronweisberg@gmail.com>
2024-10-21 19:10:47 -0700

    adds copilot source

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -29,7 +29,7 @@ return {
       vim.list_extend(opts.sources, {
         { name = "nvim_lsp", group_index = 1 },
         { name = "nvim_lsp_signature_help", group_index = 1 },
-        -- { name = "copilot", group_index = 2 },
+        { name = "copilot", group_index = 2 },
         { name = "path", group_index = 2 },
         { name = "luasnip", group_index = 2 },
         { name = "nvim_lua", group_index = 2 },
