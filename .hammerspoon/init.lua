eb6061967000c338c0d875c7ad6e45cca4d1130e
Your Name <aaronweisberg@gmail.com>
2024-10-25 10:07:38 -0700

    use hammerspoon to open urls

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,3 +1,17 @@
 require("keyboard")
 
+-- Function to open URL in specified browser
+function openURLInArc(url, browser)
+	-- Use 'open' command to specifically launch URL in Arc
+	local command = string.format('/usr/bin/open -a "%s" "%s"', browser, url)
+	hs.execute(command)
+end
+
+-- force browser focus when opening links
+-- fixes issues with slack where the link is opened, but focus doesn't change
+-- requires hammerspoon to be the default browser
+hs.urlevent.httpCallback = function(scheme, host, params, fullURL)
+	openURLInArc(fullURL, "Arc")
+end
+
 -- hs.loadSpoon("ControlEscape"):start()
