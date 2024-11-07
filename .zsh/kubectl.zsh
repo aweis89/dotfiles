d68ceed0a68d60d54cb4679b40195111979037d8
Your Name <aaronweisberg@gmail.com>
2024-11-06 16:27:33 -0800

    update

--- .zsh/kubectl.zsh
+++ .zsh/kubectl.zsh
@@ -1,46 +1,44 @@
-alias kcn=kubens
-alias kcu=kubectx
-
 delete-finalizers() {
   kubectl get pods $@ -o json | jq -r \
-    '.items[] | select(.metadata.deletionTimestamp!=null) | .metadata.name' \
-    | xargs -I {} kubectl patch pod $@ {} -p '{"metadata":{"finalizers":null}}'
+    '.items[] | select(.metadata.deletionTimestamp!=null) | .metadata.name' |
+    xargs -I {} kubectl patch pod $@ {} -p '{"metadata":{"finalizers":null}}'
 }
 
 kf() {
-    local resource=${1:-pods}
-    local name=$(kubectl get $resource --no-headers ${@:2} | fzf | awk '{print $1}')
-    local yaml=$(kubectl get $resource $name -o yaml)
-    echo $yaml | fzf_stdin_preview
+  local resource=${1:-pods}
+  local name=$(kubectl get $resource --no-headers ${@:2} | fzf | awk '{print $1}')
+  local yaml=$(kubectl get $resource $name -o yaml)
+  echo $yaml | fzf_stdin_preview
 }
 
 kfa() {
-    local resource=${1:-pods}
-    local line=$(kubectl get $resource --no-headers -A ${@:2} | fzf)
-    local namespace=$(echo $line | awk '{print $1}')
-    local name=$(echo $line | awk '{print $2}')
-    local yaml=$(kubectl get $resource $name -n $namespace -o yaml)
-    echo $yaml | fzf_stdin_preview
+  local resource=${1:-pods}
+  local line=$(kubectl get $resource --no-headers -A ${@:2} | fzf)
+  local namespace=$(echo $line | awk '{print $1}')
+  local name=$(echo $line | awk '{print $2}')
+  local yaml=$(kubectl get $resource $name -n $namespace -o yaml)
+  echo $yaml | fzf_stdin_preview
 }
 
-BAT_THEME=${BAT_THEME:-'OneHalfDark'}
+# Kubernetes Functions
+k-node-run() {
+  local node_name=$1
+  shift
+  local command=("$@")
+  local args_json=$(printf ', "%s"' "${command[@]}")
+  args_json="[${args_json:2}]"
 
-fzf_stdin_preview() {
-    local lang=${1:-yaml}
-    local surround=20
-    if test -n "$TMUX"
-    then
-        local height=$(tmux display-message -p '#{pane_height}')
-        surround=$((height - 15))
-    fi
-
-    local stdin=$(cat /dev/stdin)
-    local tmpfile=$(mktemp $HOME/tmp/fzfstin.XXXX)
-    trap "rm -f $tmpfile" EXIT
-
-    echo "$stdin" | tee $tmpfile | fzf --preview \
-    "cat $tmpfile | head -n \$(($surround+{n})) | \
-        tail -n \$(($surround*2)) | bat \
-        --language=$lang --color=always --style=grid \
-        --highlight-line=\$(($surround+1)) --theme $BAT_THEME"
+  kubectl run -it curl-pod --image=curlimages/curl --overrides="{
+        \"apiVersion\": \"v1\",
+        \"spec\": {
+            \"nodeSelector\": {
+                \"kubernetes.io/hostname\": \"${node_name}\"
+            },
+            \"containers\": [{
+                \"name\": \"curl-pod\",
+                \"image\": \"curlimages/curl\",
+                \"args\": ${args_json}
+            }]
+        }
+    }"
 }
