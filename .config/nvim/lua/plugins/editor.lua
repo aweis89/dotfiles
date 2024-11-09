e9705e6a6155388d5ea512fb9d719917b85a0001
Your Name <aaronweisberg@gmail.com>
2024-11-08 18:45:02 -0800

    update plugins

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -1,18 +1,16 @@
 vim.api.nvim_command([[command! TmuxSplitV silent execute '!tmux split-window -v -e "cd %:p:h"']])
 vim.api.nvim_command([[command! TmuxSplitH silent execute '!tmux split-window -h -e "cd %:p:h"']])
 
--- Define a function to check if dark mode is enabled
 local function is_dark_mode_enabled()
-  -- Check if the operating system is macOS
-  -- if vim.fn.system("uname") == "Darwin\n" then
-  local handle = io.popen("defaults read -g AppleInterfaceStyle 2>/dev/null")
-  if handle == nil then
-    return
+  if vim.fn.system("uname") == "Darwin\n" then
+    local handle = io.popen("defaults read -g AppleInterfaceStyle 2>/dev/null")
+    if handle == nil then
+      return
+    end
+    local result = handle:read("*a")
+    handle:close()
+    return result:match("^%s*Dark%s*$") ~= nil
   end
-  local result = handle:read("*a")
-  handle:close()
-  return result:match("^%s*Dark%s*$") ~= nil
-  -- end
 end
 
 if is_dark_mode_enabled() then
@@ -27,6 +25,11 @@ return {
     opts = function(_, opts)
       opts.colorscheme = "tokyonight"
     end,
+    init = function()
+      vim.api.nvim_create_user_command("Reload", function()
+        vim.cmd("!touch ~/.config/nvim/lua/plugins/editor.lua")
+      end, { desc = "Reload config" })
+    end,
   },
   {
     "ibhagwan/fzf-lua",
@@ -35,6 +38,7 @@ return {
       { "<leader>sg", "<cmd>FzfLua live_grep<cr>", desc = "Fzf live_grep" },
     },
   },
+  { "sindrets/diffview.nvim" },
   { "almo7aya/openingh.nvim" },
   { "christoomey/vim-tmux-navigator" },
   { "mbbill/undotree" },
