d904cfc6c50360c42de53acf404200089f074747
Your Name <aaronweisberg@gmail.com>
2024-11-29 14:57:48 -0800

    refactor: Remove unused code and simplify plugin configurations

--- .config/nvim/lua/plugins/codecompanion.lua
+++ .config/nvim/lua/plugins/codecompanion.lua
@@ -1,43 +1,3 @@
-local fmt = string.format
-
-local constants = {
-  LLM_ROLE = "llm",
-  USER_ROLE = "user",
-  SYSTEM_ROLE = "system",
-}
-
-function readFiles(directory, extension)
-  local result = {}
-
-  -- Check if directory exists
-  local cmd = string.format("test -d '%s'", directory)
-  local ret = os.execute(cmd)
-  if not ret then
-    error(string.format("Error: %s is not a valid directory", directory))
-  end
-
-  -- Find all files with the given extension
-  local find_cmd = string.format("find '%s' -type f -name '*.%s'", directory, extension)
-  local find = io.popen(find_cmd)
-  if not find then
-    error("Failed to execute find command")
-  end
-
-  -- Process each file
-  for filepath in find:lines() do
-    local cat = io.popen(string.format("cat '%s'", filepath))
-    if cat then
-      table.insert(result, "### " .. filepath .. "\n\n")
-      table.insert(result, cat:read("*all"))
-      table.insert(result, "\n\n================================================================================\n\n")
-      cat:close()
-    end
-  end
-
-  find:close()
-  return table.concat(result)
-end
-
 return {
   {
     "echasnovski/mini.diff",
@@ -51,7 +11,6 @@ return {
   },
   {
     "olimorris/codecompanion.nvim",
-    -- dir = "/Users/aaron.weisberg/p/codecompanion.nvim",
     config = true,
     opts = {
       strategies = {
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,19 +1,14 @@
-if true then
-  return {}
-end
-
 return {
   {
     "nvim-telescope/telescope.nvim",
     cmd = "Telescope",
     opts = function()
-      local actions = require("telescope.actions")
       require("telescope").setup({
         defaults = {
           mappings = {
             i = {
               ["<C-h>"] = "which_key",
-              ["<C-b>"] = require("telescope.actions.layout").toggle_preview,
+              ["<C-p>"] = require("telescope.actions.layout").toggle_preview,
               ["<C-g>"] = function(_) -- only works Telescope git_status
                 local selection = require("telescope.actions.state").get_selected_entry()
                 -- Git root command
@@ -31,7 +26,6 @@ return {
           },
           preview = {
             highlight_line = true, -- Enable line highlighting in preview
-            hide_on_startup = true,
           },
           layout_config = {
             width = 0.97, -- 97% of screen width
@@ -51,52 +45,3 @@ return {
     end,
   },
 }
-
--- keys = {
---   -- LSP
---   {
---     "<leader>ss",
---     function()
---       require("telescope.builtin").lsp_document_symbols({
---         symbols = LazyVim.config.get_kind_filter(),
---         symbol_width = 80, -- Increase this value to show more of the symbol name
---       })
---     end,
---     desc = "Goto Symbol",
---   },
-
---   {
---     "<leader>sS",
---     function()
---       require("telescope.builtin").lsp_dynamic_workspace_symbols({
---         symbols = LazyVim.config.get_kind_filter(),
---         entry_maker = function(entry)
---           local display = require("telescope.pickers.entry_display").create({
---             separator = " ",
---             items = {
---               { width = 80 }, -- symbol name
---               { width = 20 }, -- path
---             },
---           })
---           -- Get the raw symbol data
---           local symbol = entry.symbol or entry
---           local name = symbol.text or symbol.name
---           -- local kind = entry.kind
---           local filename = symbol.filename or (symbol.location and vim.uri_to_fname(symbol.location.uri)) or ""
---           local rel_filename = require("telescope.utils").transform_path({ cwd = vim.fn.getcwd() }, filename)
---           return {
---             value = symbol,
---             filename = filename,
---             display = function(_)
---               return display({
---                 name,
---                 rel_filename,
---               })
---             end,
---             ordinal = name,
---           }
---         end,
---       })
---     end,
---     desc = "Goto Symbol (Workspace)",
---   },
