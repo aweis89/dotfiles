f6c76334892075c01709e95dc73e1339e3a6f5da
Your Name <aaronweisberg@gmail.com>
2024-12-02 15:19:54 -0800

    feat: Enhance terminal keymaps and toggleterm configuration in Neovim

--- .config/nvim/lua/plugins/aider.lua
+++ .config/nvim/lua/plugins/aider.lua
@@ -4,17 +4,19 @@ return {
     "akinsho/toggleterm.nvim",
     opts = {
       shade_terminals = false,
+      -- default direction when none specified in arg to AiderToggle
       direction = "float",
       float_opts = {
         border = "curved",
         title_pos = "center",
       },
-      open_mapping = [[<c-\>]],
       close_on_exit = true,
       size = function(term)
         if term.direction == "horizontal" then
+          -- use %40 height
           return vim.o.lines * 0.4
         elseif term.direction == "vertical" then
+          -- use %40 width
           return vim.o.columns * 0.4
         end
       end,
