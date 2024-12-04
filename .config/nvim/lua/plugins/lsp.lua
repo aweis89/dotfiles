42e59c4d28cca06ee176317925c2c88f24a744f7
Your Name <aaronweisberg@gmail.com>
2024-12-03 16:54:58 -0800

    refactor: Simplify LSP configuration for gopls in Neovim

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -7,55 +7,11 @@ return {
     ---@class PluginLspOpts
     opts = {
       servers = {
-        taplo = {},
-        cmake = {},
-        jsonls = {},
-        jdtls = {},
-        vimls = {},
-        sourcekit = {},
-        -- java_language_server = {},
-        bashls = {
-          filetypes = { "sh", "zsh", "bash" },
-        },
-        html = {
-          filetypes = { "html", "templ" },
-        },
-        htmx = {
-          filetypes = { "html", "templ" },
-        },
-        templ = {},
-        tailwindcss = {
-          filetypes = { "templ", "astro", "javascript", "typescript", "react" },
-          init_options = { userLanguages = { templ = "html" } },
-        },
         gopls = {
           filetypes = { "go", "gomod", "gohtmltmpl", "gotexttmpl" },
-          message_level = vim.lsp.protocol.MessageType.Error,
-          flags = { allow_incremental_sync = true, debounce_text_changes = 1000 },
           hints = false,
           settings = {
             gopls = {
-              -- more settings: https://github.com/golang/tools/blob/master/gopls/doc/settings.md
-              -- not supported
-              analyses = {
-                unusedparams = true,
-                unreachable = true,
-                fieldalignment = false,
-              },
-              codelenses = {
-                generate = true, -- show the `go generate` lens.
-                gc_details = false, --  // Show a code lens toggling the display of gc's choices.
-                test = true,
-                tidy = true,
-              },
-              usePlaceholders = true,
-              completeUnimported = true,
-              staticcheck = true,
-              matcher = "fuzzy",
-              diagnosticsDelay = "500ms",
-              symbolMatcher = "fuzzy",
-              gofumpt = false, -- true, -- turn on for new repos, gofmpt is good but also create code turmoils
-              -- buildFlags = { "-tags", "integration" },
               hints = {
                 assignVariableTypes = false,
                 compositeLiteralFields = false,
@@ -68,9 +24,6 @@ return {
             },
           },
         },
-        kotlin_language_server = {},
-        tsserver = {},
-        -- helm_ls = {},
       },
       diagnostics = {
         float = {
