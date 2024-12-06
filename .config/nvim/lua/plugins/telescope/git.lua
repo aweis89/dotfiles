ce6c27fab18ae69f0e59af67e6eb096b8681969c
Your Name <aaronweisberg@gmail.com>
2024-12-05 22:44:57 -0800

    refactor: Extract git-related functions from telescope config into separate module

---
+++ .config/nvim/lua/plugins/telescope/git.lua
@@ -0,0 +1,63 @@
+local M = {}
+
+local previewers = require("telescope.previewers")
+
+-- Git reference diffview action
+M.git_ref_diffview_action = function()
+  local action_state = require("telescope.actions.state")
+  local selected_entry = action_state.get_selected_entry()
+  local value = selected_entry.value
+  vim.api.nvim_win_close(0, true)
+  vim.cmd("stopinsert")
+  vim.schedule(function()
+    vim.cmd(("DiffviewOpen %s^!"):format(value))
+  end)
+end
+
+-- Git reference delta previewer
+M.git_ref_delta_previewer = previewers.new_termopen_previewer({
+  get_command = function(entry)
+    return { "git", "-c", "core.pager=delta", "-c", "delta.side-by-side=false", "diff", entry.value .. "^!" }
+  end,
+})
+
+-- Git file delta previewer
+M.git_file_delta_previewer = previewers.new_termopen_previewer({
+  get_command = function(entry)
+    local is_staged = entry.status:sub(1, 1) ~= " " and entry.status:sub(1, 1) ~= "?"
+    local is_unstaged = entry.status:sub(2, 2) ~= " "
+
+    if is_staged then
+      return {
+        "git",
+        "-c",
+        "core.pager=delta",
+        "-c",
+        "delta.side-by-side=false",
+        "diff",
+        "--cached",
+        "--",
+        entry.value,
+      }
+    elseif is_unstaged then
+      return { "git", "-c", "core.pager=delta", "-c", "delta.side-by-side=false", "diff", "--", entry.value }
+    else
+      return { "bat", entry.value }
+    end
+  end,
+})
+
+-- Git add file action
+M.git_add_file = function(_)
+  local selection = require("telescope.actions.state").get_selected_entry()
+  local git_root = vim.fn.system("git rev-parse --show-toplevel"):gsub("\n", "")
+  local file_path = git_root .. "/" .. selection.value
+  local result = vim.fn.system("git add " .. file_path)
+  if result == "" then
+    vim.notify("Added file: " .. file_path)
+  else
+    vim.notify("Failed to add file: " .. file_path .. ". Error: " .. result)
+  end
+end
+
+return M
