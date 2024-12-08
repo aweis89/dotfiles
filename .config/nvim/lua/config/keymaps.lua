e80a993876d7baffcf91fd52711e312ad22e194d
Your Name <aaronweisberg@gmail.com>
2024-12-08 00:22:29 -0800

    refactor: Remove AI comment from terminal autocmd configuration

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -18,7 +18,6 @@ map("n", "<C-q>", "<cmd>q<cr>", { desc = "Quit", remap = true })
 map("n", "<C-w>i", "<cmd>only<cr>", { desc = "Make current split full screen", remap = true })
 map("n", "<leader>rr", ":!%:p<cr>", { desc = "Run current file", remap = true })
 
--- this doesn't work when terminal starts automatically on startup, maybe use event for enterinig terminal instead of openeing ai!
 vim.api.nvim_create_autocmd("TermOpen", {
   callback = function()
     local function tmap(key, val)
