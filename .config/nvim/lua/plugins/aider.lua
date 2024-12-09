43f9a6f5c65bf081956128090a7414aceb48ce8c
Your Name <aaronweisberg@gmail.com>
2024-12-09 10:30:28 -0800

    use fidget for notify

--- .config/nvim/lua/plugins/aider.lua
+++ .config/nvim/lua/plugins/aider.lua
@@ -5,14 +5,21 @@ return {
       "akinsho/toggleterm.nvim",
       "nvim-telescope/telescope.nvim",
       "willothy/flatten.nvim",
+      "j-hui/fidget.nvim",
     },
     dir = "/Users/aaron.weisberg/p/aider.nvim",
     lazy = false,
     opts = {
       update_hook_cmd = "DiffviewOpen HEAD^",
+      notify = function(msg, level, opts)
+        require("fidget").notify(msg, level, opts)
+      end,
       after_update_hook = function()
         require("diffview").open({ "HEAD^" })
       end,
+      toggleterm = {
+        direction = "tab",
+      },
     },
     keys = {
       {
