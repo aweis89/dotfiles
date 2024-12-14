b17d39e06925c84b4af49ffebe8a34ce99ba7176
Your Name <aaronweisberg@gmail.com>
2024-12-13 16:10:06 -0800

    update aider config

--- .config/nvim/lua/plugins/copilotchat.lua
+++ .config/nvim/lua/plugins/copilotchat.lua
@@ -9,22 +9,5 @@ return {
     opts = {
       model = "claude-3.5-sonnet",
     },
-    config = function(_, opts)
-      local copilot = require("CopilotChat")
-      vim.api.nvim_create_user_command("CopilotAutoCommit", function()
-        local prompt =
-          "> #git:staged\n\nWrite commit message for the change with commitizen convention. Make sure the title has maximum 50 characters and message is wrapped at 72 characters. Wrap the whole message in code block with language gitcommit."
-        copilot.ask(
-          prompt,
-          vim.tbl_extend("force", opts, {
-            callback = function(msg)
-              vim.notify(msg)
-            end,
-            selection = nil,
-          })
-        )
-      end, {})
-      copilot.setup(opts)
-    end,
   },
 }
