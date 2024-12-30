78541bdeae7edbc4aef9eeb07cf836d448d57ba1
Your Name <aaronweisberg@gmail.com>
2024-12-30 11:30:16 +0100

    feat: add format disable feature via marker file
    Add ability to disable formatting by creating a "disable-format" file in the
    project root directory. This allows projects to opt-out of automatic
    formatting when needed.
    
    Also removes Python formatter config as it's not currently used.

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,6 +1,15 @@
 return {
   {
     "stevearc/conform.nvim",
+    ---@param opts ConformOpts
+    init = function(_, opts)
+      local disable_format_file = "disable-format"
+      LazyVim.ls(vim.fn.getcwd(), function(path, name, type)
+        if name == disable_format_file then
+          LazyVim.format.enable(false)
+        end
+      end)
+    end,
     opts = function(_, opts)
       local ft = opts.formatters_by_ft
       ft.go = { "gofumpt", "goimports" }
@@ -8,7 +17,6 @@ return {
       ft.yaml = {}
       ft.lua = { "stylua" }
       ft.bash = { "bashls" }
-      ft.python = {}
       return opts
     end,
   },
