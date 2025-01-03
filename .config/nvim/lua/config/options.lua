644368e644be5a149b06d6e88c66362190f7f41a
Your Name <aaronweisberg@gmail.com>
2025-01-03 01:38:24 +0100

    feat: Add clipboard config and key remaps for Neovide

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -12,3 +12,33 @@ vim.opt.wrap = true
 vim.opt.linebreak = true
 vim.opt.breakindent = true
 vim.opt.showbreak = "↪ "
+
+-- Configure clipboard
+vim.opt.clipboard = { 'unnamed', 'unnamedplus' }
+
+-- Set up key remaps
+local keymap = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+local opts_f = { noremap = false, silent = true }
+
+-- Use 'y' to yank selected text; 'yy' to yank the current line
+
+-- Keep cursor at the bottom of the visual selection after you yank it.
+keymap('v', 'y', 'ygv<Esc>', opts)
+
+-- Prevent selecting and pasting from overwriting what you originally copied.
+keymap('x', 'p', 'pgvy', opts)
+
+if vim.g.neovide then
+  vim.api.nvim_set_keymap('v', '<sc-c>', '"+y', { noremap = true })
+  vim.api.nvim_set_keymap('n', '<sc-v>', 'l"+P', { noremap = true })
+  vim.api.nvim_set_keymap('v', '<sc-v>', '"+P', { noremap = true })
+  vim.api.nvim_set_keymap('c', '<sc-v>', '<C-o>l<C-o>"+<C-o>P<C-o>l', { noremap = true })
+  vim.api.nvim_set_keymap('i', '<sc-v>', '<ESC>l"+Pli', { noremap = true })
+  vim.api.nvim_set_keymap('t', '<sc-v>', '<C-\\><C-n>"+Pi', { noremap = true })
+  vim.g.neovide_theme = 'gruvybox'
+
+  -- modity to add italic to `Operator Mono Book Italic` and bold to `Operator Mono Medium` using these docs: https://neovide.dev/configuration.html ai!
+  vim.o.guifont = "Operator Mono Book:h21" -- text below applies for VimScript
+  vim.env.DEEPSEEK_API_KEY = 'sk-0d415e9c1f95418fa1ed4268d792d3c4'
+end
