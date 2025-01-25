d5fd207c2934c382beca0850d1435e4f428bae66
Your Name <aaronweisberg@gmail.com>
2025-01-24 20:32:55 -0800

    feat(editor): add neo-tree and mini.files plugins
    - Integrated neo-tree for file navigation.
    - Configured mini.files as the default explorer.
    - Mapped keys for opening mini.files for the current directory
      and the current file.

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -159,6 +159,38 @@ return {
   { "akinsho/bufferline.nvim", enabled = false },
   { "rcarriga/nvim-notify",    enabled = true },
   {
+    "nvim-neo-tree/neo-tree.nvim",
+    enabled = true,
+    keys = {
+      { "<leader>e", false },
+      { "<leader>E", false }
+    },
+  },
+  {
+    "echasnovski/mini.files",
+    opts = {
+      options = {
+        use_as_default_explorer = true,
+      },
+    },
+    keys = {
+      {
+        "<leader>e",
+        function()
+          require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
+        end,
+        desc = "Open mini.files (Directory of Current File)",
+      },
+      {
+        "<leader>E",
+        function()
+          require("mini.files").open(vim.uv.cwd(), true)
+        end,
+        desc = "Open mini.files (cwd)",
+      }
+    },
+  },
+  {
     "airblade/vim-rooter",
     init = function()
       vim.g.rooter_patterns = { '.git', '=nvim', '=src' }
