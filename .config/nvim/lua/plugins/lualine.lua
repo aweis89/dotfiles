b25fcd4cb85691dcdcd93d293c438f74407b9e10
Your Name <aaronweisberg@gmail.com>
2025-02-07 23:25:11 -0800

    feat: update plugin versions and add lualine config
    - Updated commits for several plugins in lazy-lock.json
    - Removed bufferline.nvim from editor plugins
    - Added new lualine configuration in lualine.lua
    - Removed git stash shortcut from snacks.lua

---
+++ .config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,25 @@
+return {
+  { "akinsho/bufferline.nvim", enabled = false },
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = {
+      sections = {
+
+        lualine_c = {
+          LazyVim.lualine.root_dir(),
+          {
+            "diagnostics",
+            symbols = {
+              error = LazyVim.config.icons.diagnostics.Error,
+              warn = LazyVim.config.icons.diagnostics.Warn,
+              info = LazyVim.config.icons.diagnostics.Info,
+              hint = LazyVim.config.icons.diagnostics.Hint,
+            },
+          },
+          { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+          { "buffers" },
+        },
+      },
+    },
+  },
+}
