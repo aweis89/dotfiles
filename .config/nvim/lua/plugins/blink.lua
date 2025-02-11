7a5f9a0b8a5cf75a3c08aed68ed967fcf7f8b877
Your Name <aaronweisberg@gmail.com>
2025-02-11 11:46:26 -0700

    feat: refactor cmdline handling in blink.lua
    Updated the cmdline function to improve command detection and
    restructured the list selection logic for better clarity.
    This change enhances the overall functionality of the blink
    plugin in Neovim.

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -110,18 +110,18 @@ return {
             end,
           },
         },
-        -- cmdline = function()
-        --   local type = vim.fn.getcmdtype()
-        --   -- Search forward and backward
-        --   if type == "/" or type == "?" then
-        --     return { "buffer" }
-        --   end
-        --   -- Commands
-        --   if type == ":" then
-        --     return { "cmdline" }
-        --   end
-        --   return {}
-        -- end,
+        cmdline = function()
+          local type = vim.fn.getcmdtype()
+          -- Search forward and backward
+          if type == "/" or type == "?" then
+            return { "buffer" }
+          end
+          -- Commands
+          if type == ":" then
+            return { "cmdline" }
+          end
+          return {}
+        end,
       },
       keymap = {
         ["<C-k>"] = { "select_prev", "fallback" },
@@ -146,6 +146,15 @@ return {
             columns = { { "label", "source_name", gap = 1 }, { "kind_icon" } },
           },
         },
+        list = {
+          selection = {
+            auto_insert = function(ctx) return ctx.mode ~= 'cmdline' end,
+            preselect = function(ctx)
+              return ctx.mode ~= 'cmdline' and not require('blink.cmp').snippet_active({ direction = 1 })
+            end,
+          },
+        },
+
       },
     },
   },
