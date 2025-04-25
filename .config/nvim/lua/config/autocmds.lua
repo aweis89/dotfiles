8509e42b622c15924f9991b0f79a9b80e257566c
Your Name <aaronweisberg@gmail.com>
2025-04-25 09:01:44 -0700

    feat: Implement automatic commit message generation
    This commit introduces automatic commit message generation using `aichat` when opening a `COMMIT_EDITMSG` buffer.
    
    The `c-msg` script now pipes the staged git diff to `aichat`, and the response is used to populate the commit message buffer.
    
    Additionally, the "ai_commit" snack in the snacks picker has been renamed to "commit" and the `c-msg` call has been removed from the snack to be handled by the autocmd.

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,27 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  group = augroup("ai_commit_msg"),
+  pattern = "COMMIT_EDITMSG",
+  callback = function()
+    vim.system({ "c-msg" }, {}, function(res)
+      vim.schedule(function()
+        if res.code ~= 0 then
+          vim.notify(res.stderr)
+          return
+        end
+        local msg = res.stdout
+        if msg == nil or msg == "" then
+          vim.notify("No commit message")
+        else
+          vim.api.nvim_buf_set_lines(0, 0, 1, false, vim.split(msg, "\n"))
+        end
+      end)
+    end)
+  end,
+})
