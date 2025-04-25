ae091d41bbc25ccc903751b4b80940b0a403b3d5
Your Name <aaronweisberg@gmail.com>
2025-04-24 18:41:11 -0700

    feat(snacks): add PR review and diff actions
    - Implemented actions for reviewing and diffing pull requests
      using the Octo.nvim plugin.
    - Added key mappings for quick access to these actions.

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -323,4 +323,65 @@ return {
       return vim.tbl_deep_extend("force", opts or {}, overrides)
     end,
   },
+  {
+    "pwntester/octo.nvim",
+    cmd = "Octo",
+    opts = {
+      picker = "snacks",
+      picker_config = {
+        mappings = {
+          -- snacks uses <c-b> for scrolling
+          open_in_browser = { lhs = "<C-u>", desc = "Open URL issue in browser" },
+        },
+        snacks = {
+          actions = {
+            pull_requests = {
+              {
+                name = "ai_review",
+                lhs = "<leader><space>r",
+                desc = "Review PR",
+                mode = { "n", "i" },
+                fn = function(p, item)
+                  local pr_num = item.number
+
+                  vim.notify("Reviewing PR #" .. pr_num)
+                  local pr_view_system = vim.system({ "gh", "pr", "view", pr_num }, {})
+                  local pr_diff_system = vim.system({ "gh", "pr", "diff", pr_num }, {})
+                  local result = pr_view_system:wait().stdout
+                  result = result .. pr_diff_system:wait().stdout
+                  local term = require("ai-terminals").get("aider")
+                  require("ai-terminals").send(
+                    "/ask Review this PR carefully\n" .. result,
+                    { term = term, submit = true }
+                  )
+                end,
+              },
+              {
+                name = "delta_diff",
+                lhs = "<leader><space>d",
+                desc = "Diff PR",
+                mode = { "n", "i" },
+                fn = function(p, item)
+                  p:close()
+                  Snacks.terminal.open(
+                    { "/bin/bash", "-c", "gh pr diff " .. item.number .. "| delta --paging=never" },
+                    {
+                      auto_close = false,
+                      win = {
+                        keys = {
+                          q = function(win)
+                            win:destroy()
+                          end,
+                        },
+                      },
+                    }
+                  )
+                end,
+              },
+            },
+          },
+        },
+      },
+    },
+  },
 }
