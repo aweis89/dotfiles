7bcf9364bbc3d89b753b9a4e5c991c43bd571cf9
Your Name <aaronweisberg@gmail.com>
2025-04-28 01:29:34 +0200

    feat(snacks): Integrate pull request picker with Octo and enhance config
    This commit integrates the pull request picker with octo.nvim,
    providing an enhanced configuration and customizable actions.
    
    The integration involves:
    
    - Using octo.nvim's API to fetch pull requests.
    - Adding new actions like opening in browser, copying URL, checking out,
      and merging pull requests.
    - Making the actions configurable via octo.nvim's config.
    - Reusing octo.nvim utils functions.
    - Adding keyboard mappings to call the PR picker.
    
    Additionally, this commit renames an autogroup in autocmds.lua to prevent naming collisions.

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -2,7 +2,7 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 local function augroup(name)
-  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+  return vim.api.nvim_create_augroup("config_" .. name, { clear = true })
 end
 
 -- Autocommand to populate commit message using c-msg
