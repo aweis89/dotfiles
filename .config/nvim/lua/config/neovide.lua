71daf7209e3a50f36bea0c7a25b1a6f148d43c34
Your Name <aaronweisberg@gmail.com>
2025-05-09 06:14:46 -0700

    chore(nvim): Configure Aider model keymaps and update Neovide font

--- .config/nvim/lua/config/neovide.lua
+++ .config/nvim/lua/config/neovide.lua
@@ -56,25 +56,8 @@ vim.opt.guicursor = {
   "r-cr-o-v:hor10",
 }
 
--- vim.g.neovide_cursor_animation_length = 0.01
--- vim.g.neovide_cursor_trail_size = 0.9
--- vim.g.neovide_cursor_unfocused_outline_width = 0.1
--- vim.g.neovide_cursor_vfx_mode = "railgun" -- railgun|torpedo|pixiedust|sonicboom|ripple|wireframe
---
--- vim.g.neovide_cursor_animate_in_insert_mode = true
--- vim.g.neovide_cursor_animate_command_line = true
---
--- -- only railgun, torpedo, and pixiedust
--- vim.g.neovide_cursor_vfx_particle_lifetime = 0.8
--- vim.g.neovide_cursor_vfx_particle_density = 20.0
--- vim.g.neovide_cursor_vfx_particle_speed = 40.0
---
--- -- only railgun
--- vim.g.neovide_cursor_vfx_particle_phase = 1.3
--- vim.g.neovide_cursor_vfx_particle_curl = 1.3
-
 vim.g.neovide_fullscreen = true
-vim.o.guifont = "Operator Mono Book,Fira Code:h20"
+vim.o.guifont = "Operator Mono,Fira Code:h20"
 vim.g.neovide_theme = "gruvybox"
 
 -- vim.api.nvim_set_keymap("i", "<d-v>", '<ESC>l"+Pli', { noremap = true })
