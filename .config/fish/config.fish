c1deb3ba6fb343a9f56377a041f29a3669da67bb
Your Name <aaronweisberg@gmail.com>
2025-08-11 08:48:34 -0700

    refactor(fish): split FZF_DEFAULT_OPTS into multiple lines for readability

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -60,7 +60,9 @@ abbr -a -- zshs 'nvim ~/.zshrc'
 
 set -gx EDITOR nvim
 set -gx VISUAL nvim
-set -gx FZF_DEFAULT_OPTS '--tmux 80% --layout=reverse --color=light --bind "tab:down,shift-tab:up,ctrl-d:preview-half-page-down,ctrl-u:preview-half-page-up" --bind="ctrl-/:change-preview-window(down,50%,border-top|hidden|)"'
+set -gx FZF_DEFAULT_OPTS '--tmux 80% --layout=reverse --color=light' \
+    '--bind "tab:down,shift-tab:up,ctrl-d:preview-half-page-down,ctrl-u:preview-half-page-up"' \
+    '--bind="ctrl-/:change-preview-window(down,50%,border-top|hidden|)"'
 
 functions -q fzf_configure_bindings && fzf_configure_bindings \
     --directory=\cf \
