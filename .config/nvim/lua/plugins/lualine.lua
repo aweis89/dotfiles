5c98a26cdbeb520495efb8e014f662f6fd45638e
Your Name <aaronweisberg@gmail.com>
2025-08-15 09:23:18 -0700

    feat(lualine): refresh statusline on FocusGained and ColorScheme by toggling laststatus

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -7,6 +7,14 @@ return {
     config = function(_, opts)
       require("lualine").setup(opts)
 
+      vim.api.nvim_create_autocmd({ "FocusGained", "ColorScheme" }, {
+        callback = function()
+          vim.defer_fn(function()
+            vim.opt.laststatus = 0
+          end, 100)
+        end,
+      })
+
       if vim.env.TMUX then
         vim.o.laststatus = 0
       end
