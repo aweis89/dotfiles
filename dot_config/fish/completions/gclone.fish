a570ed65c9eb33307da4ce54f740f8b9ad5a1012
Aaron Weisberg <aaronweisberg@gmail.com>
2025-08-19 09:21:33 -0700

    refactor(gclone): simplify completion by removing interactive query and always listing repos

--- dot_config/fish/completions/gclone.fish
+++ dot_config/fish/completions/gclone.fish
@@ -3,12 +3,7 @@
 # Complete repository names from GitHub organization using gh cli
 function __gclone_repos
     if test -n "$GITHUB_ORG"
-        set -l query (commandline -ct)
-        if test -n "$query"
-            gh repo list $GITHUB_ORG --limit 1000 --json name --jq ".[].name | select(startswith(\"$query\"))" 2>/dev/null
-        else
-            gh repo list $GITHUB_ORG --limit 100 --json name --jq '.[].name' 2>/dev/null
-        end
+        gh repo list $GITHUB_ORG --limit 1000 --json name --jq '.[].name' 2>/dev/null
     end
 end
 
