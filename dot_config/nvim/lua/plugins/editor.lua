3b979c6d8f7672ba737246accd74f9f5b3724598
Aaron Weisberg <aaronweisberg>
2025-08-20 21:41:36 -0700

    feat(editor): integrate stay-centered.nvim and auto-save.nvim with basic config
    - add arnamak/stay-centered.nvim (lazy = false)
    - add okuuva/auto-save.nvim (version ^1.0.0, cmd = "ASToggle", event = { "InsertLeave", "TextChanged" })

--- dot_config/nvim/lua/plugins/editor.lua
+++ dot_config/nvim/lua/plugins/editor.lua
@@ -9,6 +9,16 @@ return {
     version = "*",
   },
   {
+    "arnamak/stay-centered.nvim",
+    lazy = false,
+  },
+  {
+    "okuuva/auto-save.nvim",
+    version = "^1.0.0", -- see https://devhints.io/semver, alternatively use '*' to use the latest tagged release
+    cmd = "ASToggle", -- optional for lazy loading on command
+    event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
+  },
+  {
     "CopilotC-Nvim/CopilotChat.nvim",
     enabled = false,
   },
