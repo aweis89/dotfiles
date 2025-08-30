173f79f00e58357f7e5f5e2ac072efe47046b154
Aaron Weisberg <aaronweisberg>
2025-08-29 23:06:21 -0700

    chore(editor): remove auto-save.nvim plugin from configuration

--- dot_config/nvim/lua/plugins/editor.lua
+++ dot_config/nvim/lua/plugins/editor.lua
@@ -13,12 +13,6 @@ return {
     lazy = false,
   },
   {
-    "okuuva/auto-save.nvim",
-    version = "^1.0.0", -- see https://devhints.io/semver, alternatively use '*' to use the latest tagged release
-    cmd = "ASToggle", -- optional for lazy loading on command
-    event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
-  },
-  {
     "CopilotC-Nvim/CopilotChat.nvim",
     enabled = false,
   },
