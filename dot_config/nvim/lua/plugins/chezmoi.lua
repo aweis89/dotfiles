a12ff9ef6f4a654d27a8424c478b66532be8ba05
Aaron Weisberg <aaronweisberg>
2025-08-31 18:22:04 -0700

    refactor(nvim): simplify chezmoi autocmd COMMIT_EDITMSG check

--- dot_config/nvim/lua/plugins/chezmoi.lua
+++ dot_config/nvim/lua/plugins/chezmoi.lua
@@ -1,14 +1,14 @@
 return {
   {
     "xvzc/chezmoi.nvim",
+
     init = function()
       -- run chezmoi edit on file enter
       vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
         pattern = { os.getenv("HOME") .. "/.local/share/chezmoi/*" },
         callback = function()
           -- skip COMMIT_EDITMSG files
-          local buf_name = vim.api.nvim_buf_get_name(0)
-          if buf_name:match("COMMIT_EDITMSG$") then
+          if vim.fn.expand("%:t") == "COMMIT_EDITMSG" then
             return
           end
           vim.schedule(require("chezmoi.commands.__edit").watch)
