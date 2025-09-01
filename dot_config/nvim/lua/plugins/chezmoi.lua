b6f6e32fb4430704d96aeb5287f8da5b51b31815
Aaron Weisberg <aaronweisberg>
2025-08-31 17:56:48 -0700

    feat(nvim): add chezmoi plugin with autocmd to edit chezmoi files

---
+++ dot_config/nvim/lua/plugins/chezmoi.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "xvzc/chezmoi.nvim",
+    init = function()
+      -- run chezmoi edit on file enter
+      vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+        pattern = { os.getenv("HOME") .. "/.local/share/chezmoi/*" },
+        callback = function()
+          if vim.bo.filetype == "gitcommit" then
+            return
+          end
+          vim.schedule(require("chezmoi.commands.__edit").watch)
+        end,
+      })
+    end,
+  },
+}
