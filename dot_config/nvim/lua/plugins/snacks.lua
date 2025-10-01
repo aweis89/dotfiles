4d56e2576dd4972d6bb1ee57c7c0408b6a0a018c
Aaron Weisberg <aaronweisberg@gmail.com>
2025-10-01 16:47:24 -0700

    chore(nvim): remove commented fff.nvim plugin block from snacks config - delete large commented-out configuration for dmtrKovalenko/fff.nvim - keep active snacks.nvim entry and fork-related notes intact

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -97,32 +97,6 @@ local function defer_insert()
 end
 
 return {
-  -- {
-  --   "dmtrKovalenko/fff.nvim",
-  --   -- fff.nvim depends on a Rust crate that uses nightly-only features.
-  --   -- Build with nightly to avoid stable-channel feature errors.
-  --   build = "cargo build --release",
-  --   -- or if you are using nixos
-  --   -- build = "nix run .#release",
-  --   opts = { -- (optional)
-  --     debug = {
-  --       enabled = true, -- we expect your collaboration at least during the beta
-  --       show_scores = true, -- to help us optimize the scoring system, feel free to share your scores!
-  --     },
-  --   },
-  --   -- No need to lazy-load with lazy.nvim.
-  --   -- This plugin initializes itself lazily.
-  --   lazy = false,
-  --   keys = {
-  --     {
-  --       "ff", -- try it if you didn't it is a banger keybinding for a picker
-  --       function()
-  --         require("fff").find_files()
-  --       end,
-  --       desc = "FFFind files",
-  --     },
-  --   },
-  -- },
   {
     "folke/snacks.nvim",
     repo = "aweis89/snacks.nvim",
