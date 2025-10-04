d10b42da55763fc73f866b6bf2c0e01d743be877
Aaron Weisberg <aaronweisberg@gmail.com>
2025-10-03 21:01:17 -0700

    feat(hammerspoon): automate Slack workspace switching
    - Add logic to cycle through Slack workspaces to prevent idle detection
    - Restore focus to the previously active application after checking Slack
    - Use `mainWindow()` instead of `allWindows()[1]` for more reliable window targeting

--- dot_hammerspoon/lua/slack.lua
+++ dot_hammerspoon/lua/slack.lua
@@ -4,6 +4,7 @@
 local IDLE_MIN = 1 -- trigger after X minutes idle
 
 local function makeSlackActive()
+	local currentApp = hs.application.frontmostApplication()
 	hs.application.launchOrFocus("Slack")
 	local slackApp = hs.application.find("Slack")
 	if not slackApp then
@@ -12,23 +13,23 @@ local function makeSlackActive()
 	end
 
 	-- Find a window of Slack
-	local slackWindow = slackApp:mainWindow() or slackApp:allWindows()[1]
-
+	local slackWindow = slackApp:mainWindow()
 	if not slackWindow then
 		hs.alert.show("No Slack window")
 		return
 	end
-	local f = slackWindow:frame()
-	local workspaces = { "1", "2" }
 
+	local workspaces = { "1", "2" }
 	-- Loop through each workspace
 	for _, workspace in ipairs(workspaces) do
 		hs.eventtap.keyStroke({ "cmd" }, workspace)
 		hs.timer.usleep(500000) -- wait 100ms
+		local f = slackWindow:frame()
 		local clickPos = { x = f.x + 100, y = f.y + 75 }
 		hs.eventtap.leftClick(clickPos)
 		hs.timer.usleep(500000) -- wait 100ms between workspaces
 	end
+	hs.application.launchOrFocus(currentApp:name())
 end
 
 hs.timer.doEvery(30, function()
