4f43bd313c1faccfab7ddeb0f647d3ec9db46d62
Aaron Weisberg <aaronweisberg@gmail.com>
2025-10-17 14:32:04 -0700

    feat(apps): add Messages app to hyper mode mappings - add "i" keybinding to launch "Messages" in hyper mode

--- dot_hammerspoon/lua/apps.lua
+++ dot_hammerspoon/lua/apps.lua
@@ -62,6 +62,7 @@ end
 -- Default keybindings for launching apps in Hyper Mode
 local defaultHyperModeAppMappings = {
 	{ "m", "Music" }, -- "M" for "Music"
+	{ "i", "Messages" }, -- "I" for "Imessages"
 	-- { "b", "Zen Browser" }, -- "B" for "Browser"
 	{ "b", "Microsoft Edge" }, -- "B" for "Browser"
 	{ "f", "Finder" }, -- "F" for "Finder"
