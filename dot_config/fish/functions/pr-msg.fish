74b66eed54c801b275a21c2e165a67499c8e52cc
Aaron Weisberg <aaronweisberg@gmail.com>
2025-11-25 12:53:06 -0800

    feat(fish): enhance PR message function to include line changes - retrieve additions and deletions from GitHub PR data - calculate total lines changed and determine size (XS, S, M, L) - append size to the output message for better context

--- dot_config/fish/functions/pr-msg.fish
+++ dot_config/fish/functions/pr-msg.fish
@@ -1,6 +1,6 @@
 function pr-msg
     # Retrieve PR data
-    set pr (gh pr view --json url,title,number,isDraft)
+    set pr (gh pr view --json url,title,number,isDraft,additions,deletions)
     # Extract repository name
     set repo (basename -s .git (git config --get remote.origin.url))
     # Parse PR attributes using jq
@@ -8,6 +8,23 @@ function pr-msg
     set number (echo $pr | jq -r '.number')
     set url (echo $pr | jq -r '.url')
     set isDraft (echo $pr | jq -r '.isDraft')
+    set additions (echo $pr | jq -r '.additions')
+    set deletions (echo $pr | jq -r '.deletions')
+
+    # Calculate total lines changed
+    set total_lines (math $additions + $deletions)
+
+    # Determine size based on lines changed
+    if test $total_lines -lt 10
+        set size XS
+    else if test $total_lines -lt 100
+        set size S
+    else if test $total_lines -lt 500
+        set size M
+    else
+        set size L
+    end
+
     # Construct the message
     # Replace square brackets in title with parentheses to avoid markdown conflicts
     set clean_title (string replace -a '[' '(' -- $title)
@@ -17,5 +34,5 @@ function pr-msg
         set msg ":draft-pr: $msg"
     end
     # Output the message and copy to clipboard
-    echo (set -q PR_EMOJI; and echo $PR_EMOJI; or echo ":pull-request:")" $msg :pray:" | tee /dev/tty | pbcopy
+    echo (set -q PR_EMOJI; and echo $PR_EMOJI; or echo ":pull-request:")" :shirt: ($size) $msg :pray:" | tee /dev/tty | pbcopy
 end
