a1a6084aaddbe01127661fcfff5fdf0430a3446a
Aaron Weisberg <aaronweisberg@gmail.com>
2026-01-12 18:20:13 -0800

    refactor(statusline): simplify statusline configuration - remove configuration for "nvim-mini/mini.nvim"

--- dot_config/nvim/lua/plugins/statusline.lua
+++ dot_config/nvim/lua/plugins/statusline.lua
@@ -1,4 +1,5 @@
 vim.g.tpipeline_clearstl = 1
+vim.g.tpipeline_statusline = "%!tpipeline#stl#line()"
 vim.opt.fillchars = { stlnc = "─", stl = "─", vert = "│" }
 
 return {
@@ -11,37 +12,5 @@ return {
     "vimpostor/vim-tpipeline",
     dependencies = { "nvim-mini/mini.nvim" },
     lazy = false,
-  },
-  {
-    "nvim-mini/mini.nvim",
-    version = false,
-    lazy = false,
-    config = function()
-      require("mini.statusline").setup({
-        content = {
-          active = function()
-            local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
-            local git = MiniStatusline.section_git({ trunc_width = 40 })
-            local diff = MiniStatusline.section_diff({ trunc_width = 75 })
-            local diagnostics = MiniStatusline.section_diagnostics({ trunc_width = 75 })
-            local lsp = MiniStatusline.section_lsp({ trunc_width = 75 })
-            local filename = MiniStatusline.section_filename({ trunc_width = 500 })
-            local fileinfo = MiniStatusline.section_fileinfo({ trunc_width = 120 })
-            local location = MiniStatusline.section_location({ trunc_width = 75 })
-            local search = MiniStatusline.section_searchcount({ trunc_width = 75 })
-
-            return MiniStatusline.combine_groups({
-              { hl = mode_hl, strings = { mode } },
-              { hl = "MiniStatuslineDevinfo", strings = { git, diff, diagnostics, lsp } },
-              "%<", -- Mark general truncate point
-              { hl = "MiniStatuslineFilename", strings = { filename } },
-              "%=", -- End left alignment
-              { hl = "MiniStatuslineFileinfo", strings = { fileinfo } },
-              { hl = mode_hl, strings = { search, location } },
-            })
-          end,
-        },
-      })
-    end,
   },
 }
