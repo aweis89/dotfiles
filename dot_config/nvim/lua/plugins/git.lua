8598703e03ad112f91d3930d7a581549021b0d9c
Aaron Weisberg <aaronweisberg@gmail.com>
2026-02-12 21:53:55 -0800

    feat(git): enhance Gitsigns navigation and staging functionality - add key mappings for navigating to the first and last hunks - introduce commands to stage, reset, and preview hunks - enable blame functionality for lines and buffers - extend diff commands to include closing behavior on diff view
    Closes #123

--- dot_config/nvim/lua/plugins/git.lua
+++ dot_config/nvim/lua/plugins/git.lua
@@ -62,14 +62,58 @@ return {
     "lewis6991/gitsigns.nvim",
     opts = function(_, opts)
       local gs = require("gitsigns")
-      local super_on_attach = opts.on_attach
 
       opts.on_attach = function(bufnr)
         local function map(mode, l, r, desc)
-          vim.keymap.set(mode, l, r, { buffer = bufnr, desc = desc, remap = true, silent = true })
+          vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc, silent = true })
         end
+        map("n", "]H", function()
+          gs.nav_hunk("last")
+        end, "Last Hunk")
+        map("n", "[H", function()
+          gs.nav_hunk("first")
+        end, "First Hunk")
+        map({ "n", "x" }, "<leader>ghs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
+        map({ "n", "x" }, "<leader>ghr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
+        map("n", "<leader>ghS", gs.stage_buffer, "Stage Buffer")
+        map("n", "<leader>ghu", gs.undo_stage_hunk, "Undo Stage Hunk")
+        map("n", "<leader>ghR", gs.reset_buffer, "Reset Buffer")
+        map("n", "<leader>ghp", gs.preview_hunk_inline, "Preview Hunk Inline")
+        map("n", "<leader>ghb", function()
+          gs.blame_line({ full = true })
+        end, "Blame Line")
+        map("n", "<leader>ghB", function()
+          gs.blame()
+        end, "Blame Buffer")
+        map("n", "<leader>ghd", gs.diffthis, "Diff This")
+        map("n", "<leader>ghD", function()
+          gs.diffthis("~")
+        end, "Diff This ~")
+        map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
 
-        super_on_attach(bufnr)
+        map("n", "]h", function()
+          if vim.wo.diff then
+            vim.cmd.normal({ "]c", bang = true })
+            return
+          end
+          if require("unified.diff").is_diff_displayed() then
+            require("unified.navigation").next_hunk()
+            return
+          end
+          vim.notify("Gitsigns: Navigating hunks", vim.log.levels.INFO)
+          gs.nav_hunk("next")
+        end, "Next Hunk")
+        map("n", "[h", function()
+          if vim.wo.diff then
+            vim.cmd.normal({ "[c", bang = true })
+            return
+          end
+          if require("unified.diff").is_diff_displayed() then
+            require("unified.navigation").previous_hunk()
+            return
+          end
+          gs.nav_hunk("prev")
+        end, "Prev Hunk")
 
         map("n", "<leader>ghD", function()
           gs.diffthis("~")
